db55f0ac6a5cc7fbbf468e3dfc61fbaced14823d
Lucas <lucasclaudinocc@gmail.com>
2024-03-06 11:52:55 -0300

    Adding task options for handling different task categories

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    task_options();
+}
+
+fn task_options() {
+    println!("Tipo de tarefas: \n 1) Contas \n 2) Compras (Supermercado) \n 3) Compras (Varejo) \n 4) Saúde \n 5) Trabalho \n 6) Estudos \n 7) Urgentes \n");
+    let mut option = String::new();
+    io::stdin().read_line(&mut option).expect("Falha ao ler a linha");
+    println!("Você escolheu a opção: {}", option);
+}
