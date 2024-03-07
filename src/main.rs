aa9925e981f444b87ed7b72323ebdde72fc5d0c2
Lucas <lucasclaudinocc@gmail.com>
2024-03-07 10:54:03 -0300

    Adding minor changes to code logic

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,35 @@ fn main() {
 }
 
 fn task_options() {
-    println!("Tipo de tarefas: \n 1) Contas \n 2) Compras (Supermercado) \n 3) Compras (Varejo) \n 4) Saúde \n 5) Trabalho \n 6) Estudos \n 7) Urgentes \n");
+    let _task_types = [
+        "Contas",
+        "ComprasSuper",
+        "ComprasVar",
+        "Saude",
+        "Trabalho",
+        "Estudos",
+        "Urgentes",
+    ];
+
+    println!(
+    "Tipo de tarefas: 
+    \n 1) Contas 
+    \n 2) Compras (Supermercado) 
+    \n 3) Compras (Varejo) 
+    \n 4) Saúde 
+    \n 5) Trabalho 
+    \n 6) Estudos 
+    \n 7) Urgentes 
+    \n"
+    );
+
     let mut option = String::new();
+
     io::stdin().read_line(&mut option).expect("Falha ao ler a linha");
+
     println!("Você escolheu a opção: {}", option);
+}
+
+fn tasks_list() {
+    println!("Lista de tarefas");
 }
